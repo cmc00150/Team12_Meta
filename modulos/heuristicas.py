a48f5095fcd67805af4da5cdbebeac058aeacc7f
cmc00150 <cmc00150@red.ujaen.es>
2025-09-15 19:51:17 +0200

    Estilo

--- modulos/heuristicas.py
+++ modulos/heuristicas.py
@@ -1,25 +1,26 @@
 def greedy(flujos: list[list[int]], distancias: list[list[int]], candidatos: int) -> tuple[list[int], int]:
+    # Obtenemos los vectores
     v_flujos = [(i, sum(row)) for i, row in enumerate(flujos)]
     v_distancias = [(i, sum(row)) for i, row in enumerate(distancias)]
 
-    sorted_flujos = sorted(v_flujos, key=lambda tuple: tuple[1], reverse=True)
-    sorted_distancias = sorted(v_distancias, key=lambda tuple: tuple[1])
+    # Los ordenamos
+    sorted_flujos = sorted(v_flujos, key=lambda tuple: tuple[1], reverse=True) # Mayor a menor
+    sorted_distancias = sorted(v_distancias, key=lambda tuple: tuple[1]) # Menor a mayor
 
     print(sorted_flujos, sorted_distancias)
 
     permutacion = [0] * candidatos
-    for index in range(1, candidatos+1):
+    for _ in range(candidatos):
         permutacion[sorted_flujos.pop(0)[0]] = sorted_distancias.pop(0)[0]
 
     costo = 0
-    for i, target in enumerate(permutacion):
+    for i, main in enumerate(permutacion):
         aux = 0
-        for j, it in enumerate(permutacion): 
-            if i == j:
-                continue
+        for j, it in range(i+1, len(permutacion)):
             #print(flujos[i][j], "(" ,i,j ,")*", distancias[target][it], "(", target,it, ")+ ", end="", flush=True)
-            print(flujos[i][j], "*", distancias[target][it], "+ ", end="", flush=True)
-            aux += flujos[i][j] * distancias[target][it]
+            it = permutacion[j]
+            print(flujos[i][j], "*", distancias[main][it], "+ ", end="", flush=True)
+            aux += flujos[i][j] * distancias[main][it]
         print("")
         costo += aux
     return (permutacion, costo)
