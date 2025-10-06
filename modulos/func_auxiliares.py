ff5ddff9a64ace59536a43356396bb58c4dc8f4e
cmc00150 <153867792+cmc00150@users.noreply.github.com>
2025-10-06 11:05:58 +0200

    Modificación dlb

--- modulos/func_auxiliares.py
+++ modulos/func_auxiliares.py
@@ -17,45 +17,47 @@ def DLB(sol: list, costo, flujos, distancias, max_iteraciones, log: Log): # Enco
     # Con iteraciones se refiere al numero de veces que se ejecuta el algoritmo dlb o la cantidad de i
     while it <= max_iteraciones and n_factibles > 0:
         #dlb
-        for i in range(len(sol)): # Miramos cada elemento
-            menor_actual = 0 # Como solo buscamos numeros negativos (porque queremos minimizar), nos quedamos con los <0.
+        i = 0
+        menor_actual = 0 # Como solo buscamos numeros negativos (porque queremos minimizar), nos quedamos con los <0.
 
-            if factible[i] == 0: # Si hay posibilidad de mejor entro
-                improve = False # Lo ponemos a falso
-                for j in range(i+1, len(sol)+i): # Opt-2, revisamos las posibles combinaciones
-                    j = j % len(sol) # Hacemos el modulo para que no se pase
-                    efic = fact(i, j, sol, flujos, distancias) # Miramos si mejora esta combinacion
-                    costoAux=costo # Hago una copia del costo hasta el momento para comparar sin perder su valor
+        if factible[i] == 0: # Si hay posibilidad de mejor entro
+            improve = False # Lo ponemos a falso
+            for j in range(i+1, len(sol)+i): # Opt-2, revisamos las posibles combinaciones
+                j = j % len(sol) # Hacemos el modulo para que no se pase
+                efic = fact(i, j, sol, flujos, distancias) # Miramos si mejora esta combinacion
+                costoAux=costo # Hago una copia del costo hasta el momento para comparar sin perder su valor
 
-                    if efic + menor_actual < menor_actual: # Si el delta es negativo (mejora):
-                        costoAux+=efic+menor_actual # Se guarda el nuevo costo para poder mostrarlo en logs
-                        # Hacemos el intercambio
-                        aux = sol[i]
-                        sol[i] = sol[j]
-                        sol[j] = aux
-                        
-                        if factible[j] == 1: # Si hemos recuperado un no factible, ahora tenemos uno más
-                            n_factibles += 1
-                        factible[i] = factible[j] = 0 # Indicamos que por estas dos unidades se puede seguir buscando
+                if efic + menor_actual < menor_actual: # Si el delta es negativo (mejora):
+                    costoAux+=efic+menor_actual # Se guarda el nuevo costo para poder mostrarlo en logs
+                    # Hacemos el intercambio
+                    aux = sol[i]
+                    sol[i] = sol[j]
+                    sol[j] = aux
+                    
+                    if factible[j] == 1: # Si hemos recuperado un no factible, ahora tenemos uno más
+                        n_factibles += 1
+                    factible[i] = factible[j] = 0 # Indicamos que por estas dos unidades se puede seguir buscando
 
-                        it += 1
+                    it += 1
+                    i = j + 1
 
-                        log.registraCambioBLocal(i,j,sol,costoAux,it)
+                    log.registraCambioBLocal(i,j,sol,costoAux,it)
 
-                        menor_actual = efic # Escogemos este vecino (guardamos las posiciones que se cambian)
-                        improve = True # Indicamos que se ha encontrado
-                        break  # Salir del bucle j una vez que encontramos una mejora
+                    menor_actual = efic # Escogemos este vecino (guardamos las posiciones que se cambian)
+                    improve = True # Indicamos que se ha encontrado
+                    break  # Salir del bucle j una vez que encontramos una mejora
 
-                costo=costoAux # Tras pasar por todas las opciones, actualizo el costo para quedarme con el mejor
+            costo=costoAux # Tras pasar por todas las opciones, actualizo el costo para quedarme con el mejor
 
-                if not improve: # Si no se ha encontrado ninguna que mejora, vetamos esta posición poniendo un 1
-                    factible[i] = 1
-                    n_factibles -= 1
-                    if n_factibles == 0:
-                        break
-
-                if(it==max_iteraciones):
+            if not improve: # Si no se ha encontrado ninguna que mejora, vetamos esta posición poniendo un 1
+                factible[i] = 1
+                n_factibles -= 1
+                i += 1
+                if n_factibles == 0:
                     break
+
+            if(it==max_iteraciones):
+                break
                 
         if(it==max_iteraciones):
             break
