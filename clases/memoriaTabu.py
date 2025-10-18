dd62e96d5e39a7e61119eb90aac4148933723fb9
Legofano <legofania@gmail.com>
2025-10-18 19:59:38 +0200

    Arreglos en func tabú y clase

--- clases/memoriaTabu.py
+++ clases/memoriaTabu.py
@@ -1,16 +1,33 @@
 from collections import deque
-from sys import maxsize
+from sys import maxsize 
 
 class MemTabu: 
   def __init__(self, tenencia: int, n: int):
-    self.__corta = deque(maxlen=tenencia)
+    self.__viejo = 0
+    self.__nuevo = 0
+    self.__tenencia = tenencia
+    self.__corta = [0] * tenencia
     self.__larga = [0 for _ in range(n) for _ in range(n)]
   
-  def pop(self):
-    return self.__corta.popleft()
+  def eliminar(self, i, j):          
+    if (self.__nuevo - self.__viejo)**2 > 1:
+      sust = 0
+      guardo = self.__corta[self.__viejo]
+      self.__viejo = self.__viejo % len(self.__tenencia)
+      while not (guardo == (i, j) or guardo == (j, i)):
+        guardo = self.__corta[i+1]
+        i %= self.__tenencia
+        self.__corta[i+1] = sust
+        sust = guardo
+    elif self.__nuevo < self.__viejo:
+        self.__corta.pop(self.__viejo)
+        self.__viejo = self.__viejo % len(self.__tenencia)
   
   def push(self, i, j):
-    self.__corta.append((i, j))
+    self.__corta.append(self.__nuevo, (i,j))
+    self.__nuevo = self.__nuevo % len(self.__tenencia)
+    if self.__nuevo > self.__viejo:
+      self.__viejo = self.__viejo + 1 % self.__tenencia
     self.__larga[i][j] += 1
 
   def tabu(self, i, j):
@@ -18,8 +35,8 @@ class MemTabu:
     for t in self.__corta:
       if (i, j) == t or (j, i) == t:
         tabu = True
+        break
     return tabu
-
 
   def menosFrecuente(self): 
     menosFrec = ((), maxsize)
--- modulos/heuristicas.py
+++ modulos/heuristicas.py
@@ -109,40 +109,43 @@ def busqueda_tabu(flujos: list[list[int]], distancias: list[list[int]], solInici
         if factible[i] == 0: # Si i tiene posibilidad de mejora buscamos con explorar_vecinos()
             mejor_local = ()
             mejora_local = maxsize
+            improve = False
 
             for j in range(i+1, len(solInicial)+i): # Opt-2, revisamos las posibles combinaciones
                 j = j % len(solInicial) # Hacemos el modulo para que no se pase
                 mejora = fact(i, j, solInicial, flujos, distancias) # Miramos si mejora esta combinacion
+                tabu = mem.tabu(i, j)
 
-                if mejora < mejora_local: # Guardamos el mejor hasta ahora
-                    mejor_local = (i, j)
-                    mejora_local = mejora
+                if tabu and mejora >= mejora_global: #  
+                    continue
 
-                if mejora_local < mejora_global: # Si la mejora hasta ahora es menor que lo que teniamos lo devolvemos
+                if mejora < mejora_global: # Si la mejora encontrada es mejor que la global, nos movemos
                     dos_opt(solInicial, i, j) # Hacemos el intercambio
                     
                     if factible[j] == 1: # Si hemos recuperado un no factible, ahora tenemos uno más
                         n_factibles += 1
                     factible[i] = factible[j] = 0 # Indicamos que por estas dos unidades se puede seguir buscando
 
+                    mejora_global = mejora
+                    if tabu:
+                        mem.eliminar(i,j) # Si ya estaba en la list lo quitamos
+                    mem.push(i, j) # La añadimos a la memoria tabú
+                    logBusqueda.registraCambioBLocal(i, j, solInicial, costoInicial - mejora_global, it)
+                    improve = True
                     it+=1
-                    break # Salir del dlb una vez que encontramos una mejora
+                    break # Salir del for una vez que encontramos una mejora
 
-            if not mem.tabu(i, j):
-                mem.push(i, j)
+                if mejora < mejora_local: # Guardamos el mejor hasta ahora
+                    mejor_local = (i, j)
+                    mejora_local = mejora
 
-        if mejora_local < mejora_global: # Si ha habido mejora actualizamos
-            i, j = mejor_local
-            mejora_global = mejora_local
-            costoInicial+= mejora_global
-            logBusqueda.registraCambioBLocal(i, j, solInicial, costoInicial, it)
-        else: # Si no se ha encontrado ninguna que mejora, vetamos esta posición poniendo un 1
-            factible[i] = 1
-            n_factibles -= 1
-            if n_factibles == 0: # Si no nos quedan más posiciones factibles
-                factible = [0] * len(solInicial) # Reiniciamos el vector de posiciones factibles
-                i, j = mejor_local
-                solInicial = dos_opt(solInicial, i, j) # Intercambiamos para quedarnos con el mejor de los vecinos encontrados (aunque no mejore la global)
+            if not improve: # Si no se ha encontrado ninguna que mejora, vetamos esta posición poniendo un 1
+                factible[i] = 1
+                n_factibles -= 1
+                if n_factibles == 0: # Si no nos quedan más posiciones factibles
+                    factible = [0] * len(solInicial) # Reiniciamos el vector de posiciones factibles
+                    i, j = mejor_local
+                    solInicial = dos_opt(solInicial, i, j) # Intercambiamos para quedarnos con el mejor de los vecinos encontrados (aunque no mejore la global)
 
         i=(i+1)%len(solInicial) # Pasamos al siguiente elemento
 
