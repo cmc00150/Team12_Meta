c1f6d0f7fdf15d26119cea716ce285298334c4ab
cmc00150 <153867792+cmc00150@users.noreply.github.com>
2025-10-20 12:25:07 +0200

    Refactor MemTabu class and update methods
    Refactor MemTabu class to improve memory management and add new functionality.

--- clases/memoriaTabu.py
+++ clases/memoriaTabu.py
@@ -3,13 +3,10 @@ from sys import maxsize
 
 class MemTabu: 
   def __init__(self, tenencia: int, n: int):
-    self.__viejo = 0
-    self.__nuevo = 0
-    self.__tenencia = tenencia
     self.__corta = deque(maxlen=tenencia)
     self.__larga = [[0 for _ in range(n)] for _ in range(n)]    
   
-  def push(self, i, j):
+  def push(self, i, j, perm: list[int]):
     try:       
       self.__corta.remove((i,j))        # Intenta borrarlo por si ya estaba
     except:
@@ -19,18 +16,25 @@ class MemTabu:
         pass
 
     self.__corta.append((i,j))          # Siempre lo añade (estuviera o no)
-    self.__larga[i][j] += 1
+
+    for i, elem in enumerate(perm):     # Actualizamos la memoria a largo plazo (fila - unidad y col - localización)
+      self.__larga[i][elem] += 1
 
   def tabu(self, i, j) -> bool:
     return (i,j) in self.__corta or (j,i) in self.__corta
 
+  def reiniciar_corto_plazo(self):
+    self.__corta.clear()
+
   def menosFrecuente(self) -> tuple[int, int]: 
-    menosFrec = ((), maxsize)
+    perm = []
+    menosFrec = maxsize
     for i, _ in enumerate(self.__larga):
-      for j in range(i+1, len(self.__larga)):
+      for j in range(0, len(self.__larga)):
         elem = self.__larga[i][j]
-        if elem < menosFrec[1] and elem != 0:
-          menosFrec = ((i, j), elem)
+        if elem < menosFrec and elem != 0:
+          menosFrec = elem
+          perm[i] = j
     return menosFrec[0]
 
   def masFrecuente(self) -> tuple[int, int]: 
