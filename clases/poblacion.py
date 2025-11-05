2e63c61ce58f73ac64774a73a7408b36e3bf4c88
César Moreno Cabello <cmc00150@red.ujaen.es>
2025-11-05 11:07:31 +0100

    Fix cruce

--- clases/poblacion.py
+++ clases/poblacion.py
@@ -1,5 +1,3 @@
-from itertools import it
-
 from clases.individuo import Individuo
 import random
 
@@ -9,29 +7,33 @@ class Poblacion:
         self.__elites = [0] * elites
         self.__cruce = t_cruce
 
-    def _construir_hijo_MOC(self, guia: list[int], modificado: list[int], pivote: int) -> list[int]:
-        mantener = guia[:pivote] # Cojo la primera mitad
-        it_asign = it(modificado[pivote:]) # Creamos un iterador de la segunda mitad
+    def _construir_hijo(self, mantener: list[int], asignar: list[int], referencia: list[int]) -> list[int]:
         hijo = []
-        for e in modificado:
+        for e in referencia:
             if e in mantener:
                 hijo.append(e)
             else:
-                hijo.append(next(it_asign))
+                hijo.append(asignar.pop(0))
         return hijo
-    
-    def _construir_hijo_OX2(self, padre1: list[int], padre2: list[int]) -> list[int]:
-        pass
 
     def cruce(self, padre1: list[int], padre2: list[int]) -> tuple[list[int], list[int]]:
         tam = len(padre1)
-        pivote = random.randint(1, tam-1) - 1 # Va desde el primero al penúltimo, lo convertimos en índice
 
         hijos = (None, None)
         if self.__cruce == "MOC":
-            hijos[0] = self._construir_hijo_MOC(padre1, padre2, pivote)
-            hijos[1] = self._construir_hijo_MOC(padre2, padre1, pivote)
+            pivote = random.randint(1, tam-1) - 1 # Va desde el primero al penúltimo, lo convertimos en índice
+            hijos[0] = self._construir_hijo(mantener=padre1[:pivote], asignar=padre1[pivote+1:], referencia=padre2)
+            hijos[1] = self._construir_hijo(mantener=padre2[:pivote], asignar=padre2[pivote+1:], referencia=padre1)
+
         elif self.__cruce == "OX2":
-            hijos[0] = self._construir_hijo_OX2(padre1, padre2)
-            hijos[1] = self._construir_hijo_OX2(padre2, padre1)
+            asignar = []
+            mantener = []
+            for i, p in enumerate(zip(padre1, padre2)):
+                for e in p:
+                    if random.randint(1, 100) <= 50:
+                        mantener.append(e)
+                    else:
+                        asignar.append(e)
+                hijos[i] = self._construir_hijo(mantener, asignar, p)
+
         return hijos
