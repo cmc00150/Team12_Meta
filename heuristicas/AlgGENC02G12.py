bddf27c784d381ec3d965660faec6e7204fed77f
César Moreno Cabello <cmc00150@red.ujaen.es>
2025-11-10 02:01:11 +0100

    ADD algoritmo genetico

--- heuristicas/AlgGENC02G12.py
+++ heuristicas/AlgGENC02G12.py
@@ -1,2 +1,62 @@
-def evolutivo_generacional():
-    pass
+from clases.poblacion import (Poblacion, Configurador, Extractor, Individuo)
+from modulos.func_auxiliares import (fact, dos_opt)
+import random
+from multiprocessing import Process
+
+# TODO Temporizador
+def evolutivo_generacional(config: Configurador, data: Extractor):
+    poblacion = Poblacion(config, data)
+
+    ev = 0
+    p_cruce = config.prcCruce
+    p_mutacion = config.prcMutacion
+    t_cruce = config.cruce
+
+    while(ev < config.maxEvaluaciones):
+        # -- SELECCIÓN --
+        poblacion.seleccion(config.kBest) # Preparamos la población para su cruce
+
+        # -- CRUCE --
+        n = len(poblacion)
+        for i in range(0, n - (n%2), 2): # Vamos cogiendo de dos en dos
+            if random.random() < p_cruce: # Cae dentro de la probabilidad de cruce, los cruzamos
+                # 1. Cogemos a los padres
+                padre1 = poblacion[i]
+                padre2 = poblacion[i+1] 
+                # 2. Los reproducimos para obtener a sus hijos
+                hijo1 = padre1.cruce(padre2, t_cruce, data.flujos, data.distancias)
+                hijo2 = padre2.cruce(padre1, t_cruce, data.flujos, data.distancias)
+                # 3. Sustituimos a los padres a los hijos
+                poblacion[i] = hijo1
+                poblacion[i+1] = hijo2
+                # 4. Vemos si es mejor que alguno de los élites
+                poblacion.considerarElite(i)
+                poblacion.considerarElite(i+1)
+                # 5. Anotamos dos evaluaciónes (una por cada hijo)
+                ev+= 2
+        
+        if (ev < config.maxEvaluaciones):
+            break
+        
+        # -- MUTACION --
+        for i in range(n):
+            if random.random() < p_mutacion: # Si cae dentro lo mutamos, sino no hacemos nada
+                # 1. Cogemos la permutación
+                perm = poblacion[i].getPermutacion()       
+                # 2. Selecciono los genes a mutar
+                posiciones = random.choices(range(len(perm)), k=2) # Cojo dos posiciones de esta permutación
+                # 3. Le hago la mutación
+                nuevaPerm = dos_opt(perm, posiciones[0], posiciones[1]) # Los intercambio
+                # 4. Calculo el costo (parcialmente)
+                variacion = fact(posiciones[0], posiciones[1], perm, data.flujos, data.data.distancias)
+                # 5. Creo el nuevo individuo
+                poblacion[i] = Individuo(nuevaPerm, poblacion[i].getCosto() + variacion, poblacion[i].getGeneracion()+1)
+                # 6. Vemos si puede añadirse como élite
+                poblacion.considerarElite(i)
+                # 7. Anotamos una evaluación al individuo mutado
+                ev+=1 
+            
+        if (ev < config.maxEvaluaciones):
+            break
+
+    return poblacion.getMejor()
