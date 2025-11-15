238eb37b0b4473c3dde885b317fa40aaab474a11
César Moreno Cabello <cmc00150@red.ujaen.es>
2025-11-15 14:16:08 +0100

    Reestructuracion Individuo

--- clases/individuo.py
+++ clases/individuo.py
@@ -1,70 +1,64 @@
 import random
-from modulos.func_auxiliares import costo
+from modulos.func_auxiliares import (costo, aleatorio, greedy_aleatorizado, dos_opt, fact)
 
 class Individuo:
-    def __init__(self, permutacion=[], costo=-1, generacion=-1, evaluado=False):
+    def __init__(self, permutacion=[], costo=None, generacion=1):
         self.__permutacion = permutacion
         self.__costo = costo
         self.__generacion = generacion
-        self.__evaluado = evaluado
-        
+
     @staticmethod
     def cruce(padre1: "Individuo", padre2: "Individuo", cruce: str, flujos: list[list[int]], distancias: list[list[int]]) -> tuple["Individuo", "Individuo"]:
         tam = len(padre1.getPermutacion)
-        perm_hijos = ([],[]) # Permutación que acabará siendo parte de los individuos hijos
         p1 = padre1.getPermutacion
         p2 = padre2.getPermutacion
 
         if cruce == 'MOC':
-            pivote = random.randint(1,tam-2) # El pivote se escoge entre el selgundo y el penúltimo
+            pivote = random.randint(1,tam-2) # El pivote se escoge entre el segundo y el penúltimo
+            # Asignación
+            asignarhijo1 = p1[pivote+1:] # Resto de valores del padre a reasignar en el hijo
+            asignarhijo2 = p2[pivote+1:]
+        else: # Consideramos que en el resto de casos usamos el OX2
+            asignarhijo1 = []
+            asignarhijo2 = []
+            # Asignación
+            for i in range(tam):
+                # 50% de cogerlo y 50% de que no
+                if random.random() < 0.5: # Si escogemos esta posición es que la asignamos
+                    asignarhijo1.append(p1[i]) # Valores a reasignar del padre 1
+                    asignarhijo2.append(p2[i]) # Lo mismo para el padre 2
 
-            mantenerhijo1 = p1[:pivote] # Se mantienen fijos los valores del padre 1 dentro del padre 2
-            asignarhijo1 = p1[pivote:] # El resto de valores se asignan por orden
-            mantenerhijo2 = p2[:pivote] # Igual pero a la inversa
-            asignarhijo2 = p2[pivote:]
+        cont1 = cont2 = 0
+        for i in range(tam):
+            if p2[i] in asignarhijo1:
+                p2[i] = asignarhijo1[cont1]
+                cont1+=1
+            if p1[i] in asignarhijo2:
+                p1[i] = asignarhijo2[cont2]
+                cont2+=1
 
-            for i in range (0,tam):
-                if p2[i] in mantenerhijo1:
-                    perm_hijos[0].append(p2[i])
-                else:
-                    perm_hijos[0].append(asignarhijo1.pop(0))
-
-                if p1[i] in mantenerhijo2:
-                    perm_hijos[1].append(p1[i])
-                else:
-                    perm_hijos[1].append(asignarhijo2.pop())
-
-        elif cruce == 'OX2': # Cruce modificado para que dé dos hijos (en lugar de solo uno) a partir de dos padres
-            eliminarpadre2 = []
-            mantenerpadre1 = []
-            eliminarpadre1 = []
-            mantenerpadre2 = []
-
-            for i in range (0, tam):
-                if (random.randint(0,100) < 50):
-                    eliminarpadre2.append(p1[i])
-                    mantenerpadre1.append(p1[i])
-                if (random.randint(0,100) < 50):
-                    eliminarpadre1.append(p2[i])
-                    mantenerpadre2.append(p2[i])
-            
-            perm_hijos[0]=p2
-            for i in range (0,tam):
-                if (p1[i] in eliminarpadre2):
-                    perm_hijos[0][i]=mantenerpadre1.pop()
-                if (p2[i] in eliminarpadre1):
-                    perm_hijos[1][i]=mantenerpadre2.pop()
-        
         nuevaGen = -1
         if (padre1.getGeneracion >= padre2.getGeneracion): # La generación de los hijos será la siguiente a la mayor de los padres
             nuevaGen = padre1.getGeneracion + 1
         else:
             nuevaGen = padre2.getGeneracion + 1
 
-        hijo1 = Individuo(perm_hijos[0],costo(perm_hijos[0],flujos,distancias),nuevaGen, True)
-        hijo2 = Individuo(perm_hijos[1],costo(perm_hijos[1],flujos,distancias),nuevaGen, True)
+        hijo1 = Individuo(p2,costo(p2,flujos,distancias),nuevaGen)
+        hijo2 = Individuo(p1,costo(p1,flujos,distancias),nuevaGen)
 
         return (hijo1,hijo2)
+
+    def mutar(self, flujos, distancias):
+        # 1. Cogemos la permutación
+        perm = self.__permutacion
+        # 2. Selecciono los genes a mutar
+        posiciones = random.choices(range(len(perm)), k=2) # Cojo dos posiciones de esta permutación
+        # 3. Le hago la mutación
+        dos_opt(perm, posiciones[0], posiciones[1]) # Los intercambio
+        # 4. Calculo el costo (parcialmente)
+        variacion = fact(posiciones[0], posiciones[1], perm, flujos, distancias)
+        # 5. Creo el nuevo individuo
+        self.__costo += variacion
 
     @property
     def getPermutacion(self) -> list[int]:
@@ -72,8 +66,7 @@ class Individuo:
     
     @property
     def getCosto(self):
-        return (self.__costo)
-    
+        return (self.__costo)    
     
     @property
     def getGeneracion(self):
